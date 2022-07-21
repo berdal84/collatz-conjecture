1fba8069d5bbdfa8a4d1f50cb6dc3dc990435b3f
Bérenger Dalle-Cort <contact@dalle-cort.fr>
2022-07-21 15:42:54 -0400

    feat(UI): add basic UI

---
+++ README.md
@@ -0,0 +1,10 @@
+# Collatz conjecture visualiser
+
+## Introduction
+
+The goal of this project is to provide a web application able to visualize the [Collatz conjecture}(https://en.wikipedia.org/wiki/Collatz_conjecture).
+
+
+## Development
+
+I followed the instructions [here](https://getbootstrap.com/docs/5.2/getting-started/webpack/) to set up my project.
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+
+module.exports = {
+  entry: './src/js/main.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 8080,
+    hot: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [
+                  require('autoprefixer')
+                ]
+              }
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  }
+}
