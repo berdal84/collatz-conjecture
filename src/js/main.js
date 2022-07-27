4e71dd0660a33d4762014a47176e29d71bda06d5
Bérenger Dalle-Cort <contact@dalle-cort.fr>
2022-07-27 11:05:43 -0400

    optimize main.js size

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,7 @@
-import '../scss/styles.scss'
-
-// Import all of Bootstrap's JS (TODO: import only the code required)
-
 
 // Import the main module once document content is loaded
 document.addEventListener('DOMContentLoaded', async (event) => {
+    await import( '../scss/styles.scss' )
     await import('bootstrap');
     import( './app.js' ).then( (module) => {
         const app = new module.App()
